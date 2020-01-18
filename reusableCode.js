fbb0c97a04931cd50a2c29181777e9174c952890
sharhan016 <sharhan.sathar@gmail.com>
2020-01-18 12:12:09 +0530

    .

---
+++ reusableCode.js
@@ -0,0 +1,24 @@
+//  static navigationOptions = ({ navigation }) => {
+//     return {
+//         title: navigation.getParam('noteName'),
+//     };
+// };
+
+// source={{ uri: 'https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' }}
+
+ https://github.com/kgrajan12/videoplayer/blob/master/App.js
+
+
+
+var sharedBlacklist = [
+    /node_modules[\/\\]react[\/\\]dist[\/\\].*/,
+    /website\/node_modules\/.*/,
+    /heapCapture\/bundle\.js/,
+    /.*\/__tests__\/.*/
+  ];
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
